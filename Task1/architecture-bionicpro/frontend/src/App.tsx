5baa514ff33558d078edf1911c51d055b2dd0a59
Danko2000 <45896654+Danko2000@users.noreply.github.com>
2025-12-22 01:48:46 +0300

    Create App.tsx

---
+++ Task1/architecture-bionicpro/frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import ReportPage from './components/ReportPage';
+
+const keycloakConfig: KeycloakConfig = {
+  url: process.env.REACT_APP_KEYCLOAK_URL,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM || "",
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || ""
+};
+
+const keycloak = new Keycloak(keycloakConfig);
+
+// ВАЖНО: включаем PKCE на стороне keycloak-js
+const keycloakInitOptions = {
+  pkceMethod: 'S256',        // Authorization Code + PKCE
+  checkLoginIframe: false    // опционально: отключаем фоновый iframe-пинг
+};
+
+const App: React.FC = () => {
+  return (
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={keycloakInitOptions}
+    >
+      <div className="App">
+        <ReportPage />
+      </div>
+    </ReactKeycloakProvider>
+  );
+};
+
+export default App;
