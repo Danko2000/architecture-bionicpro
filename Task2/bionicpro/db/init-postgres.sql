6626e61d28fe1f1aedc561074f3b922b5054c125
Danko2000 <45896654+Danko2000@users.noreply.github.com>
2025-12-27 14:58:24 +0300

    Update init-postgres.sql

--- Task2/bionicpro/db/init-postgres.sql
+++ Task2/bionicpro/db/init-postgres.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS crm_clients_table (
     client_id SERIAL PRIMARY KEY,
-    full_name VARCHAR(255),
+    full_name VARCHAR(255), -- Это поле используется в витрине как client_name
     prosthesis_model VARCHAR(100),
     is_active BOOLEAN DEFAULT TRUE
 );
@@ -17,15 +17,21 @@ CREATE TABLE IF NOT EXISTS raw_telemetry_table (
 TRUNCATE TABLE crm_clients_table CASCADE;
 TRUNCATE TABLE raw_telemetry_table CASCADE;
 
+-- Вставка данных клиентов
+-- full_name совпадает с username из realm-export.json
 INSERT INTO crm_clients_table (client_id, full_name, prosthesis_model) VALUES 
-(1, 'Ivan Ivanov', 'Bionic-Hand-v2'),
-(2, 'Petr Petrov', 'Bionic-Leg-v1'),
-(3, 'sidorov', 'Bionic-Finger-v1');
+(1, 'prothetic1', 'Bionic-Hand-v2'), -- Соответствует пользователю prothetic1
+(2, 'prothetic2', 'Bionic-Leg-v1'),  -- Соответствует пользователю prothetic2
+(3, 'prothetic3', 'Bionic-Finger-v1');-- Соответствует пользователю prothetic3
 
+-- Вставка тестовой телеметрии
 INSERT INTO raw_telemetry_table (client_id, timestamp, response_time_ms, noise_level, duration_sec) VALUES
+-- Данные для prothetic1
 (1, current_date - interval '1 day' + interval '10 hours', 150, 0.5, 3600),
 (1, current_date - interval '1 day' + interval '14 hours', 120, 0.4, 1800),
+-- Данные для prothetic2
 (2, current_date - interval '1 day' + interval '12 hours', 200, 0.8, 7200),
+-- Данные для prothetic3
 (3, current_date - interval '1 day' + interval '10 hours', 150, 0.5, 3600),
 (3, current_date - interval '1 day' + interval '14 hours', 120, 0.4, 1800),
 (3, current_date - interval '1 day' + interval '18 hours', 100, 0.3, 1200);
